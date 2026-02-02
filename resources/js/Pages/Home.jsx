41c3f5eef8f5f901093b3dd0f359aead1054f0e7
phantriviethoang <phantriviethoang@gmail.com>
2026-02-02 11:04:27 +0700

    install inertia

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="title">Hello Word</h1>
+            <button className="btn">Button</button>
+        </>
+    );
+}
